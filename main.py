c83e0e806da69e3d0693f5c2d5266628bf7eb1bf
Huberdoggy <16614357+Huberdoggy@users.noreply.github.com>
2026-02-16 12:42:25 -0600

    Worked up through beginning of Level 4; associated queries added to dict in my_solutions. Added return type hints to the functions I wrote in my_solutions.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from solutions import instantiate, populate, run_query
 engine, tables = instantiate()
 
 # Eventually, might use this to selectively run other tests/imported functions
+# Build an evidence trail to the 'flag'
 if __name__ == "__main__":
-    # populate(engine, tables)
-    run_query(engine, key='boot_errors')
+    populate(engine, tables)  # All tables, expanded view
+    run_query(engine, db_table='boot_errors')  # 1.2
+    run_query(engine, db_table='failed_modules')  # 2.1
+    run_query(engine, db_table='ct_investigation')  # 2.2
+    run_query(engine, db_table='triple_threat')  # 3.1
+    run_query(engine, db_table='temporal_analysis')  # 3.2
--- solutions/my_solutions.py
+++ solutions/my_solutions.py
@@ -1,15 +1,62 @@
 import sqlite3
+from typing import Any
 
 import pandas as pd
 from sqlalchemy import create_engine
 
-queries = {'boot_errors': """
-    SELECT DISTINCT log_level, subsystem, message, boot_session
-    FROM boot_logs WHERE log_level LIKE 'ERR%' OR log_level LIKE 'CRIT%'
-    """}
+queries = {
+    'boot_errors': """
+    SELECT DISTINCT boot_session AS sessions_with_errors
+    FROM boot_logs WHERE log_level IN ('ERROR', 'CRITICAL')
+    ORDER BY sessions_with_errors DESC
+    """,
+    'failed_modules': """
+    SELECT module_name, COUNT(status) AS failure_count FROM module_events
+    WHERE status = 'FAILED'
+    GROUP BY module_name HAVING failure_count >= 1
+    ORDER BY failure_count DESC
+    """,
+    'ct_investigation': """
+    SELECT DISTINCT events.module_name, codes.severity AS error_severity
+    FROM module_events AS events
+    INNER JOIN error_codes AS codes
+    ON events.module_name = codes.affected_module
+    WHERE events.status = 'FAILED' AND
+    (error_severity LIKE 'CRIT%' OR error_severity LIKE 'H___')
+    ORDER BY error_severity
+    """,
+    'triple_threat': """
+    SELECT me.module_name, COUNT(DISTINCT me.event_id) AS failed_loads,
+    COUNT(DISTINCT ec.error_id) AS crit_errors,
+    COUNT(DISTINCT mem.mem_id) AS mem_failures
+    FROM module_events AS me
+    INNER JOIN error_codes AS ec
+    ON me.module_name = ec.affected_module
+    AND ec.severity LIKE 'CRIT%'
+    INNER JOIN memory_events AS mem
+    ON me.module_name = mem.requesting_module
+    AND mem.allocation_success != 'True'
+    WHERE me.status = 'FAILED'
+    GROUP BY me.module_name
+    HAVING COUNT(DISTINCT me.event_id) > 0
+    AND COUNT(DISTINCT ec.error_id) > 0
+    AND COUNT(DISTINCT mem.mem_id) > 0
+    ORDER BY (COUNT(DISTINCT me.event_id) + COUNT(DISTINCT ec.error_id) +
+    COUNT(DISTINCT mem.mem_id)) DESC
+    """,
+    'temporal_analysis': """
+    SELECT DISTINCT module_name FROM module_events as t1
+    INNER JOIN system_calls AS t2
+    ON t1.module_name = t2.caller_module
+    WHERE t1.boot_session IN (2, 3)
+    AND t2.return_code < 0
+    AND ABS(t1.timestamp - t2.timestamp) < 100
+    ORDER BY t1.module_name
+    """,
+}
 
 
-def instantiate_db():
+def instantiate_db() -> list[Any, list[str]]:
     """
     PRELIMINARY WORK:
     Set universally applicable viewing preferences; retrieve top-level view of all tables;
@@ -29,7 +76,7 @@ def instantiate_db():
     return [engine, tables]  # Unpack in main.py
 
 
-def dump_tables(engine, tables: list[str, None]):
+def dump_tables(engine, tables: list[str, None]) -> None:
     """
     CHALLENGE 1.1:
     I preferred to start from scratch, installing pandas and sqlalchemy in order to render
@@ -49,7 +96,7 @@ def dump_tables(engine, tables: list[str, None]):
         print(f"Caught an error - {type(e).__name__}: {str(e)}")
 
 
-def run_query(engine, **kwargs):
+def run_query(engine, heading: str = None, **kwargs) -> None:
     """
     Reusable implementation for various challenges - unpack associated keyword args
     values as lookup keys for 'queries' dictionary
@@ -57,10 +104,17 @@ def run_query(engine, **kwargs):
     if len(kwargs.items()) == 0:
         print("No key specified to lookup a query.")
         return
+        # For now, I'll only ever pass desired dict key/s as kwargs
     try:
         for v in kwargs.values():
             query = queries.get(v, "Not found.")
             df_query = pd.read_sql_query(query, con=engine)
-            print(df_query)
+            heading = (
+                f"{'-' * 50} {str(v.upper())} {'-' * 50}"
+                if not heading
+                else heading.upper()  # Defaults to formatted query name
+            )
+            print(heading)
+            print(df_query, end="\n\n")
     except Exception as e:
         print(f"Caught an error - {type(e).__name__}: {str(e)}")
